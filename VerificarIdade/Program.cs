766ad19a8b103794cb3870774f593d90177f369f
Gabriel de Morais <morais.g99@gmail.com>
2023-06-25 16:40:44 -0300

    Adicionar arquivos de projeto.

---
+++ VerificarIdade/Program.cs
@@ -0,0 +1,21 @@
+﻿class VerificarIdade
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade < 18)
+        {
+            Console.WriteLine("Você não pode dirigir por ser menor de idade.");
+        }
+        else if (idade >= 18 && idade < 60)
+        {
+            Console.WriteLine("Você é um adulto.");
+        }
+        else
+        {
+            Console.WriteLine("Você é um idoso.");
+        }
+    }
+}
